f9b108075e3fbca2834987a21891beeeb1e8e13c
Luckybox59 <Luckybox59@gmail.com>
2019-09-03 19:34:41 +0500

    update

--- common.js
+++ common.js
@@ -6,3 +6,19 @@ $(document).ready(function () {
     autoplay: true,
   });
 });
+
+const buttonCart = document.querySelector('a[data-target="#modal-cart"]');
+buttonCart.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { target } = e.currentTarget.dataset;
+  console.log(target);
+  const modalCart = document.querySelector(target);
+  modalCart.style = 'display:flex;';
+  modalCart.addEventListener('click', (e) => {
+    if (e.target.closest('.modal-content')) {
+      e.stopPropagation();
+      return;
+    }
+      e.target.style = 'display:none;';
+  });
+});
